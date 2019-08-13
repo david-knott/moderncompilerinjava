d2e9b6967d73e83e9430ddc206d14adb3e038682
April McDonnell <mrdavidknott@gmail.com>
2019-08-13 11:30:34 +0100

    moved files around

---
+++ src/chap1/exercises/ex1_1.java
@@ -0,0 +1,8 @@
+
+
+class Ex1_1 {
+
+    public static void main(String[] args){
+    
+    }
+}
--- src/chap1/programming/interp.java
+++ src/chap1/programming/interp.java
@@ -41,13 +41,14 @@ class IntAndTable {
 }
 
 public class interp {
+
   static void interp(Stm s) {
     var table = interpStm(s, null);
     System.out.println(table); 
   }
 
   static IntAndTable interpExp(Exp e, Table t){
-    System.out.println("interpExp Exp " + e);
+ //   System.out.println("interpExp Exp " + e);
     if(e instanceof IdExp){
       var ne = (IdExp)e;
       var id = ne.id;
@@ -85,35 +86,55 @@ public class interp {
   static IntAndTable interpExp(ExpList el, Table t){
     if(el instanceof PairExpList){
       PairExpList pel = (PairExpList)el;
-      System.out.println("interpStm PairExpList");
-      return interpExp(pel.head, interpExp(pel.tail, t).t);
+      var res = interpExp(pel.head, interpExp(pel.tail, t).t);
+      return res;
     }
     if(el instanceof LastExpList){
       LastExpList lel = (LastExpList)el;
-      System.out.println("interpStm LastExpList");
-      return interpExp(lel.head, t);
+      var res = interpExp(lel.head, t);
+      return res;
     }
     throw new RuntimeException("This shouldn't happen " + el.getClass().getName());
   }
-
+  
   static Table interpStm(Stm s, Table t){
     if(s instanceof CompoundStm){
       CompoundStm cs = (CompoundStm)s;
-      System.out.println("interpStm CompoundStm");
       return interpStm(cs.stm2, interpStm(cs.stm1, t));
     }
     if(s instanceof AssignStm){
       //create a new value in the symbol table.
       AssignStm cs = (AssignStm)s;
-      System.out.println("interpStm AssignStm");
       var assignResult = interpExp(cs.exp, t);
       //set the assignment value and the result from the expression
       return new Table(cs.id, assignResult.i, assignResult.t);
     }
     if(s instanceof PrintStm){
+      //show print the contents of the evaulated expressions in cs.exps
+      //we first expand out the exppression list and pass in a reference to 
+      //the current table
       PrintStm cs = (PrintStm)s;
-      IntAndTable it = interpExp(cs.exps, t);
-      System.out.println("interpStm PrintStm: " + it.i);
+      ExpList expList = cs.exps;
+      Exp nextExp = null;
+      ExpList nextExpList = null;
+      Table currentTable = t;
+      //keep iterating while exp list is a pair of expressions
+      while(expList instanceof PairExpList){
+        //expand out the pair expression list to current expression and expression list
+        nextExp = ((PairExpList)expList).head;
+        nextExpList = ((PairExpList)expList).tail;
+        //evalue the current exp
+        var intAndTable = interpExp(nextExp, currentTable);
+        System.out.print(intAndTable.i);
+        //set the current table to the result of the interpExp function
+        currentTable = intAndTable.t;
+        //set the list to the current expression list, and repeat until no more pair of expresion lists 
+        expList = nextExpList;
+      }
+      //the last item must be a last exp list item.
+      nextExp =  ((LastExpList) expList).head;
+      IntAndTable it = interpExp(nextExp, currentTable);
+      System.out.println(it.i);
       return it.t;
     }
     throw new RuntimeException("This shouldn't happen " + s.getClass().getName());
