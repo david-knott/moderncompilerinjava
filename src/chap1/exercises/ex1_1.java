3d62a57190c04df8acca7570549b2861e67b0fb9
April McDonnell <mrdavidknott@gmail.com>
2019-08-13 11:40:26 +0100

    exercise 1.1

--- src/chap1/exercises/ex1_1.java
+++ src/chap1/exercises/ex1_1.java
@@ -1,8 +1,44 @@
+import javax.lang.model.util.ElementScanner6;
 
+class Tree {
+    Tree left;
+    String key;
+    Tree right;
+
+    Tree(Tree l, String k, Tree r) {
+        left = l;
+        key = k;
+        right = r;
+    }
+
+    public String toString(){
+        String s = "[Tree ";
+
+        s+= "]";
+        return s;
+    }
+
+}
 
 class Ex1_1 {
 
-    public static void main(String[] args){
     
+    public static Tree insert(String key, Tree t) {
+        if (t == null)
+            return new Tree(null, key, null);
+        else if (key.compareTo(t.key) < 0)
+            return new Tree(insert(key, t.left), t.key, t.right);
+        else if (key.compareTo(t.key) > 0)
+            return new Tree(t.left, t.key, insert(key, t.right));
+        else
+            return new Tree(t.left, key, t.right);
+    }
+
+    public static void main(String[] args) {
+        var t1 = new Tree(null, "a", null);
+        var t2 = insert("b", t1);
+        var t3 = insert("c", t2);
+        var t4 = insert("d", t3);
+        System.out.println(t4);
     }
 }
