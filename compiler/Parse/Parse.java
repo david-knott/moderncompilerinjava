2ffec875d8a991cd707f16bef3f907cd3b1d496c
David Knott <mrdavidknott@gmail.com>
2019-10-29 23:50:50 +0000

    ast

--- compiler/Parse/Parse.java
+++ compiler/Parse/Parse.java
@@ -84,7 +84,7 @@ public class Parse {
 		try {
 			java_cup.runtime.Symbol rootSymbol = parser.parse();
 			Program root = (Program) rootSymbol.value;
-			// new Absyn.Print(System.out).prExp(root.absyn, 0);
+			 new Absyn.Print(System.out).prExp(root.absyn, 0);
 		} catch (Throwable e) {
 			e.printStackTrace();
 			throw new Error(e.toString());
