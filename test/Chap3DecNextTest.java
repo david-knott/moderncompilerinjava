e93b623af785929e7b8ceca3ab16679301837180
David Knott <mrdavidknott@gmail.com>
2019-11-01 20:16:56 +0000

    next test

---
+++ test/Chap3DecNextTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import Parse.*;
+
+public class Chap3DecNextTest {
+
+    @Test
+    public void associativeTest() {
+        String filename = "./test/fixtures/decnext.tig";
+        new Parse(filename);
+    }
+}
