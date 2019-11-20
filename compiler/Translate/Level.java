173036106d6e6e4bcf92bb04e56b47c9079cdef4
David Knott <davek@communicraft.com>
2019-11-20 15:24:57 +0000

    added some comments

--- compiler/Translate/Level.java
+++ compiler/Translate/Level.java
@@ -4,14 +4,21 @@ import Symbol.Symbol;
 import Util.BoolList;
 
 public class Level {
+    //stack frame associate with this level
     Frame.Frame frame;
+    //formal function arguments for level
     public AccessList formals;
     public Level(Level parent, Symbol name, BoolList fmls){
 
     }
+
+    //associate a frame with this level
     public Level(Frame.Frame f){
 
     }
+    //allocate new local in the current frame
+    //probaby called by accessing the symbol table
+    //when a new variable is declared
     public Access allocLocal(boolean escape){
         return null;
     }
