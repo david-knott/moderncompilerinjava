c3234ac3eeac47a5c1ec40738f7beabc82c51495
David Knott <mrdavidknott@gmail.com>
2020-03-26 19:32:27 +0000

    formatting

--- compiler/RegAlloc/InterferenceGraph.java
+++ compiler/RegAlloc/InterferenceGraph.java
@@ -1,10 +1,16 @@
 package RegAlloc;
+
 import Graph.Node;
 import Graph.Graph;
 
 abstract public class InterferenceGraph extends Graph {
    abstract public Node tnode(Temp.Temp temp);
+
    abstract public Temp.Temp gtemp(Node node);
+
    abstract public MoveList moves();
-   public int spillCost(Node node) {return 1;}
+
+   public int spillCost(Node node) {
+      return 1;
+   }
 }
