42078bb7ec98fefada78ae6c7b18e6634652f8e1
David Knott <mrdavidknott@gmail.com>
2020-06-11 09:12:49 +0100

    added tests

---
+++ test/E2E/tests/pplus.tig.s
@@ -0,0 +1,33 @@
+.globl tigermain
+.text
+tigermain:
+pushq %rbp
+movq %rsp, %rbp
+addq $0, %rsp
+# start main
+L1:
+movl %ebx, %ebx # default move
+movl %r12d, %r12d # default move
+movl %r13d, %r13d # default move
+movl %r14d, %r14d # default move
+movl %r15d, %r15d # default move
+movl $3, %eax # const
+movl %eax, %eax # default move
+movl $4, %ecx # const
+movl %ecx, %ecx # default move
+movl %eax, %eax # add lexp -> r
+add %ecx, %eax
+movl %eax, %edi
+call itoa # exp call ( no return value )
+movl %r15d, %r15d # default move
+movl %r14d, %r14d # default move
+movl %r13d, %r13d # default move
+movl %r12d, %r12d # default move
+movl %ebx, %ebx # default move
+jmp L0
+L0:
+
+# end main
+movq %rbp, %rsp
+popq %rbp
+ret
