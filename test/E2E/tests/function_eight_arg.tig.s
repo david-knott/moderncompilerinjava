288cc3fcbd9f04a824b69e05d0fa961a041f6bcc
David Knott <mrdavidknott@gmail.com>
2020-06-14 19:11:18 +0100

    eight arg function test

---
+++ test/E2E/tests/function_eight_arg.tig.s
@@ -0,0 +1,94 @@
+.global tigermain
+.text
+tigermain:
+pushq %rbp
+movq %rsp, %rbp
+addq $0, %rsp
+# start main
+L2:
+movq %rbx, %rbx # default move
+movq %r12, %r12 # default move
+movq %r13, %r13 # default move
+movq %r14, %r14 # default move
+movq %r15, %r15 # default move
+movq $8, %rax # const
+pushq %rax # move arg to stack
+movq $7, %r10 # const
+pushq %r10 # move arg to stack
+movq $6, %r11 # const
+pushq %r11 # move arg to stack
+movq $5, %r9 # const
+movq %r9, %r9 # move arg to temp
+movq $4, %r8 # const
+movq %r8, %r8 # move arg to temp
+movq $3, %rcx # const
+movq %rcx, %rcx # move arg to temp
+movq $2, %rdx # const
+movq %rdx, %rdx # move arg to temp
+movq $1, %rsi # const
+movq %rsi, %rsi # move arg to temp
+movq %rbp, %rdi # move arg to temp
+call L0 # default call
+movq %rax, %rax # rax to temp 
+movq %rax, %rdi # move arg to temp
+call itoa # exp call ( no return value )
+movq %r15, %r15 # default move
+movq %r14, %r14 # default move
+movq %r13, %r13 # default move
+movq %r12, %r12 # default move
+movq %rbx, %rbx # default move
+jmp L1
+L1:
+# sink 
+# end main
+movq %rbp, %rsp
+popq %rbp
+retq
+.text
+L0:
+pushq %rbp
+movq %rsp, %rbp
+addq $-8, %rsp
+# start main
+L4:
+movq %rdi, -8(%rbp) # store to offset
+movq %rsi, %rsi # default move
+movq %rdx, %rdi # default move
+movq %rcx, %r10 # default move
+movq %r8, %r8 # default move
+movq %r9, %r9 # default move
+movq 16(%rbp), %rax # load to offset
+movq 24(%rbp), %rcx # load to offset
+movq 32(%rbp), %rdx # load to offset
+movq %rbx, %rbx # default move
+movq %r12, %r11 # default move
+movq %r13, %r12 # default move
+movq %r14, %r13 # default move
+movq %r15, %r14 # default move
+movq %rsi, %rsi # add lexp -> r
+add %rdi, %rsi
+movq %rsi, %rsi # add lexp -> r
+add %r10, %rsi
+movq %rsi, %rsi # add lexp -> r
+add %r8, %rsi
+movq %rsi, %rsi # add lexp -> r
+add %r9, %rsi
+movq %rsi, %rsi # add lexp -> r
+add %rax, %rsi
+movq %rsi, %rax # add lexp -> r
+add %rcx, %rax
+movq %rax, %rax # add lexp -> r
+add %rdx, %rax
+movq %rax, %rax # default move
+movq %r14, %r15 # default move
+movq %r13, %r14 # default move
+movq %r12, %r13 # default move
+movq %r11, %r12 # default move
+movq %rbx, %rbx # default move
+jmp L3
+L3:
+# sink 
+# end main
+movq %rbp, %rsp
+popq %rbp
+retq
