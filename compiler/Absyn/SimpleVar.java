1c796e1c5de733bdecc002c0842927d346398928
Bonzo Dog <david-knott@users.noreply.github.com>
2020-06-16 12:26:57 +0100

    Update SimpleVar.java

--- compiler/Absyn/SimpleVar.java
+++ compiler/Absyn/SimpleVar.java
@@ -4,12 +4,19 @@ import Symbol.Symbol;
 
 public class SimpleVar extends Var {
    public Symbol name;
+   public boolean readonly = false;
 
    public SimpleVar(int p, Symbol n) {
       pos = p;
       name = n;
    }
-
+   
+   public SimpleVar(int p, Symbol n, boolean ro) {
+      pos = p;
+      name = n;
+      readonly = ro;
+   }
+   
    @Override
    public void accept(AbsynVisitor visitor) {
       visitor.visit(this);
