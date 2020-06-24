a62dc43e16472c0f18a596dd0d01fd963cf8c38e
David Knott <mrdavidknott@gmail.com>
2020-06-24 07:48:48 +0100

    logical not

--- test/E2E/TestDriver.java
+++ test/E2E/TestDriver.java
@@ -20,6 +20,6 @@ public class TestDriver {
 
     @Test
     public void test2() throws FileNotFoundException {
-        Main.main(new String[]{"./test/E2E/good/string_substr.tig"});
+        Main.main(new String[]{"./test/E2E/good/int_not.tig"});
     }
 }
