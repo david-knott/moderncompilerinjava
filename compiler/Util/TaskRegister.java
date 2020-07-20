10bdbe0d39f5b7fa037391d3eba3836aea59f595
David Knott <mrdavidknott@gmail.com>
2020-07-20 17:34:39 +0100

    task register

---
+++ compiler/Util/TaskRegister.java
@@ -0,0 +1,17 @@
+package Util;
+
+public class TaskRegister {
+    public static TaskRegister taskRegister = new TaskRegister();
+
+    private TaskRegister() {
+    }
+
+    public void register(Task task) {
+    }
+
+    public void parseArgs(String[] args) {
+    }
+
+    public void execute() {
+    }
+}
