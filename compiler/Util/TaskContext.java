6977971d23b8b9f1aa17693957a4e47fc106d4aa
David Knott <mrdavidknott@gmail.com>
2020-08-06 16:14:37 +0100

    new task files

---
+++ compiler/Util/TaskContext.java
@@ -0,0 +1,11 @@
+package Util;
+
+import Parse.Program;
+
+public class TaskContext {
+    public Program program;
+
+    public void setAst(Program value) {
+        this.program = value;
+    }
+}
