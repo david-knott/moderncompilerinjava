fe68257ef21b8a157b5cc18f1351999689b7e6f8
David Knott <mrdavidknott@gmail.com>
2020-08-21 09:12:09 +0100

    commented out gcc in compile bash

--- test/E2E/compile.sh
+++ test/E2E/compile.sh
@@ -6,5 +6,5 @@ rm -rf ./good/*.out
 rm -rf ./good/*.s
 $JAVA_HOME/javac -sourcepath ../../compiler/ -d ../../bin ../../compiler/**/*.java
 $JAVA_HOME/java -cp ../../bin Main.Main $1 > $1.s
-gcc -g -w -no-pie -Wimplicit-function-declaration -Wl,--wrap,getchar $1.s ./runtime.c -o $1.out
-./$f.out
+#gcc -g -w -no-pie -Wimplicit-function-declaration -Wl,--wrap,getchar $1.s ./runtime.c -o $1.out
+#./$f.out
