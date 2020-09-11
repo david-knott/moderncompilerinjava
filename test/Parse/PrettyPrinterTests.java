dbe98b60ec872898e83d301670dc2c07df012850
David Knott <mrdavidknott@gmail.com>
2020-09-11 08:00:54 +0100

    added some unit tests for pretty printer

--- test/Parse/PrettyPrinterTests.java
+++ test/Parse/PrettyPrinterTests.java
@@ -1,9 +1,12 @@
 package Parse;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 
 import org.junit.Test;
 
@@ -12,8 +15,17 @@ import ErrorMsg.ErrorMsg;
 
 public class PrettyPrinterTests {
 
+    private final ByteArrayOutputStream baos;
+
+    public PrettyPrinterTests() {
+        this.baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        System.setOut(ps);
+    }
+
     @Test
     public void nestedParens() {
+        this.baos.reset();
         ErrorMsg errorMsg = new ErrorMsg("errorRecovery", System.out);
         String tiger = "((((((((((0))))))))))";
         InputStream targetStream = new ByteArrayInputStream(tiger.getBytes());
@@ -21,5 +33,32 @@ public class PrettyPrinterTests {
         PrettyPrinter prettyPrinter = new PrettyPrinter(System.out, true, true);
         program.absyn.accept(prettyPrinter);
         assertTrue(!errorMsg.anyErrors);
+        assertEquals(this.baos.toString(), "(\n    0\n)");
+    }
+
+    @Test
+    public void booleanAndOpSugar() {
+        this.baos.reset();
+        ErrorMsg errorMsg = new ErrorMsg("errorRecovery", System.out);
+        String tiger = "1 = 1 & 2 = 2";
+        InputStream targetStream = new ByteArrayInputStream(tiger.getBytes());
+        Program program = new CupParser(targetStream, errorMsg).parse();
+        PrettyPrinter prettyPrinter = new PrettyPrinter(System.out, true, true);
+        program.absyn.accept(prettyPrinter);
+        assertTrue(!errorMsg.anyErrors);
+        assertEquals(this.baos.toString(), "if 1 = 1 \nthen 2 = 2\nelse 0");
+    }
+
+    @Test
+    public void booleanOrOpSugar() {
+        this.baos.reset();
+        ErrorMsg errorMsg = new ErrorMsg("errorRecovery", System.out);
+        String tiger = "1 = 1 | 2 = 2";
+        InputStream targetStream = new ByteArrayInputStream(tiger.getBytes());
+        Program program = new CupParser(targetStream, errorMsg).parse();
+        PrettyPrinter prettyPrinter = new PrettyPrinter(System.out, true, true);
+        program.absyn.accept(prettyPrinter);
+        assertTrue(!errorMsg.anyErrors);
+        assertEquals(this.baos.toString(), "if 1 = 1 \nthen 1\nelse 2 = 2");
     }
 }
