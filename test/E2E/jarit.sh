f51695c692b2943212e0cba3978c813538c16919
David Knott <mrdavidknott@gmail.com>
2020-09-15 07:53:29 +0100

    added jar script

---
+++ test/E2E/jarit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+JAVA_HOME=/home/david/bin/jdk-14+36/bin
+#JAVA_HOME=/usr/bin
+$JAVA_HOME/javac -sourcepath ../../compiler/ -d ../../bin ../../compiler/**/*.java
+#$JAVA_HOME/jar cfe tc.jar ../../bin Main.Main  
+$JAVA_HOME/jar --create --file tc.jar --main-class Main.Main -C ../../ bin
+
