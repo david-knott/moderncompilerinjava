54829a98bb1d2685c0d2c4ca65b726a0a8ca1afa
David Knott <mrdavidknott@gmail.com>
2020-09-15 07:51:05 +0100

    jar script

--- test/Bind/RenamerTest.java
+++ test/Bind/RenamerTest.java
@@ -32,7 +32,7 @@ public class RenamerTest {
     @Test
     public void test4_recfunction() {
         Parser parser = new CupParser(
-                "let function foo() : int = bar() function bar() : int = foo() function foobar() : int = let function foofoo() : int = 1 in 1 end in 0 end",
+                "let function foo() : int = bar() function bar() : int = foo() function foobar() : int = let function foofoo() : int = bar() in 1 end in 0 end",
                 new ErrorMsg("", System.out));
         Program program = parser.parse();
         PrintStream outputStream = System.out;
@@ -45,4 +45,23 @@ public class RenamerTest {
         PrettyPrinter prettyPrinter = new PrettyPrinter(System.out, false, false);
         program.absyn.accept(prettyPrinter);
     }
+
+    @Test
+    public void test4_string() {
+        Parser parser = new CupParser(
+                "let type a = { a: string } function a(a: a): a = a { a = a.a + a.a } var a : a := a(a{a = \"\"}) in a.a end",
+                new ErrorMsg("", System.out));
+        Program program = parser.parse();
+        PrintStream outputStream = System.out;
+        ErrorMsg errorMsg = new ErrorMsg("", outputStream);
+        Binder binder = new Binder(errorMsg);
+
+        program.absyn.accept(binder);
+        Renamer renamer = new Renamer();
+        program.absyn.accept(renamer);
+        PrettyPrinter prettyPrinter = new PrettyPrinter(System.out, false, false);
+        program.absyn.accept(prettyPrinter);
+    }
+
+
 }
